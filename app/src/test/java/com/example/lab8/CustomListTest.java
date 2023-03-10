ad5d718279cce1007e3e04d889c907cefb5668b8
ianmccullough1001 <97405878+ianmccullough1001@users.noreply.github.com>
2023-03-09 18:13:46 -0700

    fixing text has city

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -13,28 +13,30 @@ import java.util.ArrayList;
 
 public class CustomListTest {
     CustomList list;
-    public CustomList MockCityList(){
-        list = new CustomList(null,new ArrayList<>());
+
+    public CustomList MockCityList() {
+        list = new CustomList(null, new ArrayList<>());
         return list;
     }
 
     @Test
-    public void addCityTest(){
+    public void addCityTest() {
         list = MockCityList();
         int listSize = list.getCount();
         list.addCity(new City("Estevan", "SK"));
-        assertEquals(list.getCount(),listSize + 1);
+        assertEquals(list.getCount(), listSize + 1);
     }
 
     /**
-    * In this test I am checking if it has a specific city
+     * In this test I am checking if it has a specific city
      */
     @Test
-    public void TestHasCity(){
+    public void TestHasCity() {
         list = MockCityList();
         City city = new City("Estevan", "Sak");
         list.add(city);
-        assertEquals(true,list.hasCity(city));
+        assertEquals(true, list.hasCity(city));
     }
+}
 
 
