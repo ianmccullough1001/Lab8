f50a9c7bbc23ba55c6fc61d34d4a8f6f8ccd277f
ianmccullough1001 <97405878+ianmccullough1001@users.noreply.github.com>
2023-03-09 18:32:49 -0700

    fixing text has city

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -60,6 +60,13 @@ public class CustomList extends ArrayAdapter<City> {
         return false;
 
     }
-    public void delete(City city){}
+    public void delete(City city){
+        if(cities.contains(city)){
+            cities.remove(city);
+        }else{
+            throw new IllegalArgumentException();
+        }
+
+    }
 
 }
